cf4bceaa9164d4b554e521a95f49d1907b437136
ottolol <alexsveshnik@gmail.com>
2025-05-01 12:55:16 +0300

    добавил status, selectStatus, setAppStatusAC

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -9,10 +9,14 @@ export const appSlice = createSlice({
   },
   selectors: {
     selectThemeMode: (state) => state.themeMode,
+    selectStatus: (state) => state.status,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
       state.themeMode = action.payload.themeMode
+    }),
+    setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
+      state.status = action.payload.status
     }),
   }),
 })
