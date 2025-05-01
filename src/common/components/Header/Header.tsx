dc472d194ed816e61209a19bccefe988bfbc6fb7
ottolol <alexsveshnik@gmail.com>
2025-05-01 13:00:04 +0300

    Добавил const status = useAppSelector(selectStatus)

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { changeThemeModeAC, selectThemeMode } from "@/app/app-slice.ts"
+import { changeThemeModeAC, selectStatus, selectThemeMode } from "@/app/app-slice.ts"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { containerSx } from "@/common/styles"
 import { getTheme } from "@/common/theme"
@@ -13,6 +13,7 @@ import LinearProgress from "@mui/material/LinearProgress"
 
 export const Header = () => {
   const themeMode = useAppSelector(selectThemeMode)
+  const status = useAppSelector(selectStatus)
 
   const dispatch = useAppDispatch()
 
