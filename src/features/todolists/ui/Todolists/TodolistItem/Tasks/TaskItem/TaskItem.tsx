904f21d32ad854dc3be2c22a894a26101a75ca0f
ottolol <alexsveshnik@gmail.com>
2025-05-01 16:15:32 +0300

    Обновил AC на санку - changeTaskStatusTC

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -1,6 +1,11 @@
 import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import { useAppDispatch } from "@/common/hooks"
-import { changeTaskStatusTC, changeTaskTitleAC, deleteTaskTC } from "@/features/todolists/model/tasks-slice"
+import {
+  changeTaskStatusTC,
+  changeTaskTitleTC,
+  deleteTaskTC,
+  updateTaskTC,
+} from "@/features/todolists/model/tasks-slice"
 import DeleteIcon from "@mui/icons-material/Delete"
 import Checkbox from "@mui/material/Checkbox"
 import IconButton from "@mui/material/IconButton"
@@ -25,16 +30,29 @@ export const TaskItem = ({ task, todolistId }: Props) => {
   const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
     const newStatusValue = e.currentTarget.checked
     dispatch(
-      changeTaskStatusTC({
+      updateTaskTC({
         todolistId,
         taskId: task.id,
-        status: newStatusValue ? TaskStatus.Completed : TaskStatus.New,
+        domainModel: { status: newStatusValue ? TaskStatus.Completed : TaskStatus.New },
       }),
     )
   }
 
   const changeTaskTitle = (title: string) => {
-    dispatch(changeTaskTitleAC({ todolistId, taskId: task.id, title }))
+    dispatch(
+      updateTaskTC({
+        todolistId,
+        taskId: task.id,
+        domainModel: {
+          title, // Обновляемое поле
+          description: task.description,
+          status: task.status,
+          priority: task.priority,
+          startDate: task.startDate,
+          deadline: task.deadline,
+        },
+      }),
+    )
   }
 
   const isTaskCompleted = task.status === TaskStatus.Completed
